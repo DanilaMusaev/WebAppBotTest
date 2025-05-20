8b3f2e2c3f3094ab30ad551dff67b4cba4d89e53
Danila Musaev <musaev.danilka@gmail.com>
2025-05-20 18:04:55 +0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css';
+// Вынос объекта подключенного web app для tg
+const tg = window.Telegram.WebApp;
+
+function App() {
+    useEffect(() => {
+        // Говорит тг, что приложение полностью прогрузилось
+        tg.ready();
+    });
+
+    // Функция для закрытия приложения
+    const onClose = () => {
+        tg.close();
+    };
+
+    return (
+        <div className="app">
+            Working
+            <button onClick={onClose}>Закрыть</button>
+        </div>
+    );
+}
+
+export default App;
