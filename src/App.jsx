c36e2f0fdf80f9a7daa9a82526a36ed505fd50bb
Danila Musaev <musaev.danilka@gmail.com>
2025-05-20 18:22:39 +0300

    third

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { useEffect } from 'react';
 import './App.css';
 // Вынос объекта подключенного web app для tg
 const tg = window.Telegram.WebApp;
 
 function App() {
+  
     useEffect(() => {
         // Говорит тг, что приложение полностью прогрузилось
         tg.ready();
