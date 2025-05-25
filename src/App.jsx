abb4d3ce12dd0427ed7a7eb4c1b482ae1b810ec4
Danila Musaev <musaev.danilka@gmail.com>
2025-05-25 17:22:59 +0300

    addedMainButtonToggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,21 @@
 import { useEffect } from 'react';
 import './App.css';
-// Вынос объекта подключенного web app для tg
-const tg = window.Telegram.WebApp;
+import { useTelegram } from './hooks/useTelegram';
+import Header from './components/Header/Header';
 
 function App() {
-  
+    const { tg, onToggleButton } = useTelegram();
+
     useEffect(() => {
         // Говорит тг, что приложение полностью прогрузилось
         tg.ready();
     }, []);
 
-    // Функция для закрытия приложения
-    const onClose = () => {
-        tg.close();
-    };
-
     return (
         <div className="app">
+            <Header />
             Working
-            <button onClick={onClose}>Закрыть</button>
+            <button onClick={onToggleButton}>toggle</button>
         </div>
     );
 }
