db79fd5a7040f962d9d8d7df53387c5851de2fb3
Danila Musaev <musaev.danilka@gmail.com>
2025-05-25 17:24:20 +0300

    addHeader

---
+++ src/hooks/useTelegram.js
@@ -0,0 +1,27 @@
+// Вынос объекта подключенного web app для tg
+const tg = window.Telegram.WebApp;
+
+/**
+ * Хук, который достает объект для работы с web app для tg. Также предоставляет получение некоторых часто используемых функций
+ */
+export function useTelegram() {
+    // Функция для закрытия приложения
+    const onClose = () => {
+        tg.close();
+    };
+    // Функция, чтобы скрыть/показать кнопку, предоставляемую TG
+    const onToggleButton = () => {
+        if (tg.MainButton.isVisible) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    };
+
+    return {
+        onClose,
+        onToggleButton,
+        tg,
+        user: tg.initDataUnsafe?.user,
+    };
+}
